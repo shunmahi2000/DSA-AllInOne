9e8362ea7fb0f32894d3baa1e7df0d66d01aa33f
Himanshu Yadav <himanshuyadav@Himanshus-Laptop.local>
2025-02-24 10:23:05 +0530

    Tree/LCA

--- tree/lca_binary_lifting.cpp
+++ tree/lca_binary_lifting.cpp
@@ -3,77 +3,88 @@ using namespace std;
 #define ll long long
 #define forn(i, e) for (ll i = 0; i < e; i++)
 
-void dfs(ll par, ll cur, vector<vector<ll>> &adj, vector<vector<ll>> &dp, vector<ll> &depth)
-{
-    dp[cur][0] = par;
-    depth[cur] = depth[par] + 1;
-    for (ll i = 1; i < 21; i++)
-    {
-        dp[cur][i] = dp[dp[cur][i - 1]][i - 1];
+void dfs(int cur,int par,vector<int> &depth,vector<vector<int>> &dp,vector<vector<int>> &adj){
+    dp[cur][0]=par;
+    depth[cur]=depth[par]+1;
+    for(int i=1;i<21;i++){
+        dp[cur][i]=dp[dp[cur][i-1]][i-1];
     }
-    for (auto &it : adj[cur])
-    {
-        if (it != par)
-        {
-            dfs(cur, it, adj, dp, depth);
+
+    for(auto &it:adj[cur]){
+        if(it!=par){
+            dfs(it,cur,depth,dp,adj);
         }
     }
 }
-ll lca(ll x, ll y, vector<vector<ll>> &dp, vector<ll> &depth)
-{
-    if (x == y)
-        return x;
-    if (depth[x] > depth[y])
-        swap(x, y);
-    int diff = abs(depth[x] - depth[y]);
-    for (int i = 0; i < 21; i++)
-    {
-        if (diff & (1 << i))
-        {
-            y = dp[y][i];
-        }
+int lca(int x,int y,vector<int> &depth,vector<vector<int>> &dp){
+    if(x==y) return x;
+
+    if(depth[x]>depth[y]){
+        swap(x,y);
     }
-    if (x == y)
-        return x;
-    for (ll i = 20; i >= 0; i--)
-    {
-        if (dp[x][i] != dp[y][i])
-        {
-            x = dp[x][i];
-            y = dp[y][i];
+
+    int diff=depth[y]-depth[x];
+    int ind=0;
+    while(diff){
+        if(diff&1){
+            y=dp[y][ind];
+        }
+        ind++;
+        diff=diff>>1;
+    }
+    if(x==y) return x;
+    for(int i=20;i>=0;i--){
+        if(dp[x][i]!=dp[y][i]){
+            x=dp[x][i];
+            y=dp[y][i];
         }
     }
     return dp[x][0];
+}
+vector<int> find_lca(int n,vector<vector<int>> &e,vector<vector<int>> & q){
+    vector<vector<int>> adj(n);
+    for(int i=0;i<e.size();i++){
+        adj[e[i][0]].push_back(e[i][1]);
+        adj[e[i][1]].push_back(e[i][0]);
+    }
+
+    vector<int> depth(n,0);
+    vector<vector<int>> dp(n,vector<int>(21,0));
+
+    dfs(0,0,depth,dp,adj);
+
+    vector<int> ans;
+    forn(i,q.size()){
+        ans.push_back(lca(q[i][0],q[i][1],depth,dp));
+    }
+    return ans;
+
 }
 void solve()
 {
     ll n;
     cin >> n;
-    vector<vector<ll>> e;
+    vector<vector<int>> edges;
     forn(i, n - 1)
     {
-        ll x, y;
+        int x, y;
         cin >> x >> y;
-        e.push_back({x, y});
+        edges.push_back({x, y});
     }
-    vector<vector<ll>> adj(n + 1);
-    vector<ll> depth(n + 1, 0);
-    for (ll i = 0; i < e.size(); i++)
-    {
-        adj[e[i][0] + 1].push_back(e[i][1] + 1);
-        adj[e[i][1] + 1].push_back(e[i][0] + 1);
-    }
-
-    vector<vector<ll>> dp(n + 1, vector<ll>(21, 0));
-    dfs(0, 1, adj, dp, depth);
     ll q;
     cin >> q;
-    forn(i, q)
-    {
-        ll x, y;
-        cin >> x >> y;
-        cout << lca(x + 1, y + 1, dp, depth) - 1 << endl;
+    vector<vector<int>> queries;
+    forn(i,q){
+        int x,y;
+        cin>>x>>y;
+        queries.push_back({x,y});
     }
+    
+    vector<int> ans = find_lca(n,edges,queries);
+    forn(i,ans.size()){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
 }
 int main()
 {
